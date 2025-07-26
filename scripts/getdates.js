de46b4fa58444bc7514c446d54ad19a5111749ad
ornela mbanga <orneladivinembangantsimba@gmail.com>
2025-07-26 14:07:56 -0700

    Remove files from Git tracking but keep them locally

--- scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-// Dynamically set the current year
-document.querySelector("#currentyear").textContent = new Date().getFullYear();
-
-// Display the last modified date
-document.querySelector("#lastModified").textContent = "Last Modified: " + document.lastModified;
-
--- temples.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const nav = document.querySelector(".navigation");
-
-menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("show");
-});
